debe41cf7e671767f357fab45afb6b2f3614a989
alexandar89934 <aleksandar.radovanovic2712@gmail.com>
2022-08-03 12:44:09 +0200

    meetup app completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import Layout from './components/layout/Layout';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
-   <div></div>
+   <Layout>
+
+  
+     <Routes>
+
+      <Route path='/' element={<AllMeetupsPage /> }  />
+      
+      
+      <Route path='/new-meetup' element={ <NewMeetupPage/> }/>
+
+      <Route path='/favorites' element={ <FavoritesPage/> }/>
+
+     </Routes>
+
+   
+
+
+   </Layout>
   );
 }
 
 export default App;
+
+
